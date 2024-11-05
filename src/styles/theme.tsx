3af46bae8efe30735db7864d80e2214b667870e8
hodu42 <knn330@naver.com>
2024-11-05 15:51:52 +0900

    eslint 삭제로 인한 코드 리팩토링

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,8 +1,6 @@
 import { extendTheme } from '@chakra-ui/react'
-import { config } from 'typescript-eslint'
 
 const theme = extendTheme({
-  config,
   fonts: {
     heading: `'Heading Font Name', Pretendard`,
     body: `'Body Font Name', Pretendard`,
