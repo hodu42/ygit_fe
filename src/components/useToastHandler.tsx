9ede03de45ab5730e3766b3a8aec79bb4636c700
hodu42 <knn330@naver.com>
2024-11-05 22:39:18 +0900

    신규 토스트 코드 추가

---
+++ src/components/useToastHandler.tsx
@@ -0,0 +1,19 @@
+import { useToast } from '@chakra-ui/react';
+
+const useToastHandler = () => {
+  const toast = useToast();
+
+  const showToast = (title: string, description: string, status: 'success' | 'error') => {
+    toast({
+      title: title,
+      description: description,
+      status: status,
+      duration: 6000,
+      isClosable: true,
+    });
+  };
+
+  return showToast;
+};
+
+export default useToastHandler;
