6468f6772803e3783d794409d0cd3478c05f1c18
hodu42 <knn330@naver.com>
2024-11-29 19:48:07 +0900

    회원가입, form submit 되도록 변경

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -16,7 +16,9 @@ export const Register = (): React.JSX.Element => {
   const showToast = useToastHandler();
   const isPhone = useBreakpointValue({base: true, xl: false})
 
-  const handleRegister = async () => {
+  const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
     let response: AxiosResponse<any, any>;
     try {
       response = await axios.post(`${BASE_URL}/register`, {
@@ -57,74 +59,76 @@ export const Register = (): React.JSX.Element => {
               Register
             </Text>
           </Flex>
-          <Flex flexDirection="column" alignItems="center">
-            <Text pl="2rem" width="80%" ml="2rem" fontSize="1.6rem" fontFamily="Pretendard" fontWeight="600">
-              ID
-            </Text>
-            <Input
-              type="text"
-              bg="#f4f6f9"
-              borderColor="transparent"
-              pl="3%"
-              width="80%"
-              height="50px"
-              placeholder={isPhone ? '' : "사용하실 아이디를 입력하세요."}
-              fontSize="1.3rem"
-              fontWeight={600}
-              focusBorderColor="#0dcbe4"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-            />
-          </Flex>
-          <Flex mt="4rem" flexDirection="column" alignItems="center">
-            <Text pl="2rem" width="80%" ml="2rem" fontSize="1.6rem" fontFamily="Pretendard" fontWeight="600">
-              PW
-            </Text>
-            <InputGroup width="80%" mt="1rem" position="relative">
+          <form onSubmit={handleRegister}>
+            <Flex flexDirection="column" alignItems="center">
+              <Text pl="2rem" width="80%" ml="2rem" fontSize="1.6rem" fontFamily="Pretendard" fontWeight="600">
+                ID
+              </Text>
               <Input
-                type={show ? 'text' : 'password'}
+                type="text"
                 bg="#f4f6f9"
                 borderColor="transparent"
                 pl="3%"
-                pr="4.7rem"
-                width="100%"
+                width="80%"
                 height="50px"
-                placeholder={isPhone ? '' : "사용하실 비밀번호를 입력하세요."}
+                placeholder={isPhone ? '' : "사용하실 아이디를 입력하세요."}
                 fontSize="1.3rem"
                 fontWeight={600}
                 focusBorderColor="#0dcbe4"
-                value={pw}
-                onChange={(e) => setPw(e.target.value)}
+                value={id}
+                onChange={(e) => setId(e.target.value)}
               />
-              <InputRightElement width="4.5rem" height="50px" position="absolute" display="flex" alignItems="center">
-                <Button
-                  h="40px"
-                  bg="#e2e8f0"
-                  onClick={() => setShow(!show)}
-                  _hover={{ backgroundColor: '#cbd5e0' }}
+            </Flex>
+            <Flex mt="4rem" flexDirection="column" alignItems="center">
+              <Text pl="2rem" width="80%" ml="2rem" fontSize="1.6rem" fontFamily="Pretendard" fontWeight="600">
+                PW
+              </Text>
+              <InputGroup width="80%" mt="1rem" position="relative">
+                <Input
+                  type={show ? 'text' : 'password'}
+                  bg="#f4f6f9"
+                  borderColor="transparent"
+                  pl="3%"
+                  pr="4.7rem"
+                  width="100%"
+                  height="50px"
+                  placeholder={isPhone ? '' : "사용하실 비밀번호를 입력하세요."}
                   fontSize="1.3rem"
-                >
-                  {show ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </Flex>
-          <Flex position="relative" justifyContent="center" alignItems="center" pt="5rem">
-            <Button
-              bg="#0DCBE4"
-              color="white"
-              borderColor="transparent"
-              fontSize="1.8rem"
-              height="50px"
-              _hover={{ backgroundColor: '#0DA3E4' }}
-              onClick={handleRegister}
-              sx={{
-                padding: '30px 80px',
-              }}
-            >
-              Register
-            </Button>
-          </Flex>
+                  fontWeight={600}
+                  focusBorderColor="#0dcbe4"
+                  value={pw}
+                  onChange={(e) => setPw(e.target.value)}
+                />
+                <InputRightElement width="4.5rem" height="50px" position="absolute" display="flex" alignItems="center">
+                  <Button
+                    h="40px"
+                    bg="#e2e8f0"
+                    onClick={() => setShow(!show)}
+                    _hover={{ backgroundColor: '#cbd5e0' }}
+                    fontSize="1.3rem"
+                  >
+                    {show ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Flex>
+            <Flex position="relative" justifyContent="center" alignItems="center" pt="5rem">
+              <Button
+                type='submit'
+                bg="#0DCBE4"
+                color="white"
+                borderColor="transparent"
+                fontSize="1.8rem"
+                height="50px"
+                _hover={{ backgroundColor: '#0DA3E4' }}
+                sx={{
+                  padding: '30px 80px',
+                }}
+              >
+                Register
+              </Button>
+            </Flex>
+          </form>
         </Flex>
       </Flex>
     </Flex>
