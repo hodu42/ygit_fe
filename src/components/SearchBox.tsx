2685274af265896b6b04e2c4ba6119a367e6ffd9
hodu42 <knn330@naver.com>
2024-12-01 17:23:20 +0900

    태그로 검색시 쿠키 안 보내지는거 해결

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -38,9 +38,7 @@ export const SearchBox: React.FC<SearchBoxProps>  = ({searchKeyword, setSearchKe
     try {
       // 1. 태그로 검색하여 이미지 정보 가져오기
       const searchResponse = await axios.post<ImgIdx[]>(`${BASE_URL}/search-by-tags`, searchTagList, {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-        },
+        withCredentials: true
       });
 
       // 2. 이미지 데이터를 저장할 배열
